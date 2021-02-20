d3175c46c0095518a0bb1ad8f0b417d1f63372ce
Alessandro De Leo <adv.deleo@gmail.com>
2021-02-20 00:49:38 -0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# SNEK
+
+snekDB is a lightweight local database which allows the user to store json data locally. Snek is meant to be readable, easy to use, and quick. Feel free to use and improve.
+
---
+++ snek.py
@@ -0,0 +1,77 @@
+""" 
+Ultra lightweight local python database using json
+
+feel free to make better!!!
+"""
+
+import os
+import shutil
+import json
+
+class snek():
+
+    VERSION = "0.0.1"
+
+    # Variables
+
+    INFO = {}
+    DIR = "data"
+
+    # CONFIG
+
+    def directory(self): return self.DIR
+    def set_directory(self, directory): self.DIR = directory
+
+    # Core Functions
+
+    def create(self):
+        boiler_plate = {"version": self.VERSION, "Databases": {}, "stats": {"writes": 0, "reads": 0, "databases": 0}}
+
+        try:
+            os.mkdir(self.DIR)
+            with open(f"{self.DIR}/info.json", "w+") as f:
+                f.write(json.dumps(boiler_plate))
+                f.close()
+        except Exception as e:
+            print(e)
+
+    def uninstall(self):
+        message = f"""
+        WARNING
+
+        THIS WILL DELETE {self.DIR} DIRECTORY
+        THIS ACTION CANNOT BE UNDONE PROCEED WITH CAUTION
+
+        continue? (y/n)
+        """
+        print(message)
+        answ = input("> ")
+        if answ.upper() in ["Y", "YES"]: shutil.rmtree(self.DIR)
+        else: pass # TODO HANDLE ELSE
+
+    def info(self):
+
+        info_dir = f"{self.DIR}/info.json"
+
+        if os.path.exists(self.DIR): INFO = json.load(open(info_dir))
+        else: self.create() 
+
+        return self.INFO
+
+    def create_db(self, name, init_value={}):
+        try:
+            with open(f"{self.DIR}/{name}.json", "w+") as f:
+                f.write(json.dumps(init_value))
+                f.close()
+        except Exception as e:
+            print(e)
+
+
+
+    def __init__(self):
+        self.info()
+        cmd = input("PRESS ENTER...")
+        self.uninstall()
+
+if __name__ == "__main__":
+    snek = snek()
