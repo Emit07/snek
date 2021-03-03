4e0cc538cf57439ec1319453bfbfbb701a8e300e
Ale De Leo <aledeleo@Student-adeleo25.hsd1.ca.comcast.net>
2021-03-03 12:15:37 -0800

    more work on seperating files

--- error_handeler.py
+++
@@ -1,7 +0,0 @@
-
-
-class Handler:
-
-    def check_if_none(value):
-        if value is not None: return {"passed": True}
-        else: return {"passed": False}
--- wizard.py
+++
@@ -1,9 +0,0 @@
-
-from snek import snek
-
-if __name__ == "__main__":
-    cmd = input("> ")
-    snek.insert("test", cmd, {"_id": 123456})
-    results = snek.query("test", cmd)
-    print(results)
-    
